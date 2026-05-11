fbde28df3adb33d792dd7ca5ee31a17206fb2911
Slávek Banko <slavek.banko@axis.cz>
2026-05-11 22:02:28 +0200

    yealink-module: Use timer_delete_sync instead of del_timer_sync for kernel >= 6.1.0 This resolves FTBFS with kernel >= 6.15.
    Signed-off-by: Slávek Banko <slavek.banko@axis.cz>

--- yealink.c
+++ yealink.c
@@ -91,13 +91,17 @@
 #error "Need kernel version 2.6.18 or higher"
 #endif
 
-#define DRIVER_VERSION	"20230131"
+#define DRIVER_VERSION	"20260510"
 #define DRIVER_AUTHOR	"Thomas Reitmayr, Henk Vergonet"
 #define DRIVER_DESC	"Yealink phone driver"
 
 #define USB_YEALINK_VENDOR_ID	0x6993
 #define USB_YEALINK_PRODUCT_ID1	0xb001
 #define USB_YEALINK_PRODUCT_ID2	0xb700
+
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6,1,0)
+#define timer_delete_sync del_timer_sync
+#endif
 
 /* Timeout used for synchronous reads from interrupt endpoint */
 #define YEALINK_USB_INT_TIMEOUT		200	/* in [ms] */
@@ -1921,7 +1925,7 @@ static void stop_traffic(struct yealink_dev *yld)
 	usb_kill_urb(yld->urb_irq);
 	usb_kill_urb(yld->urb_ctl);
 	if (yld->timer_active) {
-		del_timer_sync(&yld->timer);
+		timer_delete_sync(&yld->timer);
 		yld->timer_active = 0;
 	}
 
@@ -2016,7 +2020,7 @@ static void input_close(struct input_dev *dev)
 	yld->shutdown = 1;
 	smp_wmb();			/* make sure other CPUs see this */
 	if (yld->timer_active) {
-		del_timer_sync(&yld->timer);
+		timer_delete_sync(&yld->timer);
 		yld->timer_active = 0;
 	}
 	yld->shutdown = 0;
